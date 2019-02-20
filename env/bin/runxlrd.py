c8c1f36ac8d7abfb6a9e2fa67a193cec5972efc8
LAB-pesquisa <roger_amaros@hotmail.com>
2019-02-20 17:23:39 -0300

    200220191723

--- env/bin/runxlrd.py
+++ env/bin/runxlrd.py
@@ -1,4 +1,4 @@
-#!/home/lab-pesquisa/Desktop/projetos/Graficos Site /env/bin/python3
+#!/home/lab-pesquisa/Desktop/projetos/graficos_planilha/env/bin/python3
 # Copyright (c) 2005-2012 Stephen John Machin, Lingfo Pty Ltd
 # This script is part of the xlrd package, which is released under a
 # BSD-style licence.
--- graficos_planilha.py
+++ graficos_planilha.py
@@ -1,7 +1,9 @@
 #!./env/bin/python
+import seaborn as sns
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
+from functools import reduce
 
 xls = pd.ExcelFile("Ficha de Monitoramento e Avaliação dos PMEs 2018_final.xls")
 #print(xls.sheet_names)
@@ -14,7 +16,41 @@ df1 = pd.read_excel(xls,'Meta 1')
 
 years= [ x for x in df1.loc[13][3:15]]
 #------- indicador 1A ------#
-meta_prevista_1A =[ x for x in df1.loc[14][3:15]]
+# meta_prevista_1A =[ x for x in df1.loc[14][3:15]]
+meta_prevista_1A = [x for x in df1.loc[14][3:15]]
+for i in meta_prevista_1A:
+    if np.isnan(i):
+        meta_prevista_1A[meta_prevista_1A.index(i)]=0
+        
 meta_executada_1A =[ x for x in df1.loc[15][3:15]]
+for i in meta_executada_1A:
+    if np.isnan(i):
+        meta_executada_1A[meta_executada_1A.index(i)]=0
 
-print(years[2])
+meta_executada_1A = [ x*100 for x in meta_executada_1A]
+meta_prevista_1A = [ x*100 for x in meta_prevista_1A]
+
+
+
+width = 0.2
+labels = [ str(x) for x in years]
+ind = np.arange(len(labels))
+
+bar_prevista = plt.bar(ind + width,meta_prevista_1A, width, color='g', label='Meta prevista')
+bar_executada = plt.bar(ind,meta_executada_1A, width, color='r', label='Meta Executada')
+
+fig = plt.gcf()
+fig.set_size_inches(18.5, 10.5)
+
+
+
+location = ind+width/2
+labels=labels
+plt.xticks(location, labels)
+
+plt.legend()
+plt.grid()
+plt.ylabel("Execução ( em %)")
+plt.xlabel("Ano")
+fig.savefig('test2png.png', dpi=100)
+plt.show()
